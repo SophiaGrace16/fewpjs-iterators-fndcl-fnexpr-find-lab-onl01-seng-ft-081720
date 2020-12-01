c644c83f8bf94aef3e36bbf1e119235471ba2139
SophiaGrace16 <sophia.g.white@outlook.com>
2020-12-01 10:09:52 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  const found = record.find(element => element.result === "W")
+  if (found) {
+    return found.year
+  } 
+}
